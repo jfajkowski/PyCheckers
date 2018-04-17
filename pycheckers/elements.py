30094322931968989b2a611b3fd6c5b02c77496b
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-04-17 23:26:02 +0200

    SetUp Board

--- pycheckers/elements.py
+++ pycheckers/elements.py
@@ -1,12 +1,14 @@
+from typing import Tuple
+
 import numpy as np
 
 class Color:
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
+    DARK = (0, 0, 0)
+    LIGHT = (255, 255, 255)
 
 
 class Piece:
-    def __init__(self, color: Color):
+    def __init__(self, color: Tuple[int, int, int]):
         self._color = color
         self._king = False
 
@@ -14,7 +16,28 @@ class Piece:
     def is_king(self):
         return self._king
 
+class Square:
+    def __init__(self, color: Tuple[int, int, int]):
+        self._color = color
+        self._piece = None
+
+    def is_occupied(self):
+        return self._piece is not None
+
+    def is_available(self):
+        return self._color is not Color.LIGHT
+
 
 class Board:
+    SIZE = 10
+
     def __init__(self):
-        self._state = np.empty((10, 10), dtype=Piece)
+        self._state = np.empty((Board.SIZE, Board.SIZE), dtype=Square)
+
+    def setUp(self):
+        for i in range(Board.SIZE):
+            for j in range(Board.SIZE):
+                if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1):
+                    self._state[i][j] = Square(Color.DARK)
+                else:
+                    self._state[i][j] = Square(Color.LIGHT)

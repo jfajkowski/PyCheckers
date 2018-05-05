74fa39bc056ace04f33cf5356fc6224f8cbd5b93
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-05-05 17:46:43 +0200

    Implement displaying elements

--- pycheckers/elements.py
+++ pycheckers/elements.py
@@ -1,11 +1,14 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
+import pygame
 
 
 class Color:
-    DARK = (0, 0, 0)
-    LIGHT = (255, 255, 255)
+    LIGHT_PIECE = (0, 0, 0)
+    DARK_PIECE = (255, 255, 255)
+    LIGHT_SQUARE = (121, 85, 72)
+    DARK_SQUARE = (62, 39, 35)
 
 
 class Board:
@@ -13,20 +16,22 @@ class Board:
     FILLED_ROWS = 3
 
     def __init__(self):
-        self.squares = np.empty((Board.SIZE, Board.SIZE), dtype=Square)
-        self.light_pieces = []
-        self.dark_pieces = []
+        self.state = np.empty((Board.SIZE, Board.SIZE), dtype=Square)
+        self.light_pieces: List[Piece] = []
+        self.dark_pieces: List[Piece] = []
+        self._build()
+        self.prepare_pieces()
 
-    def build(self):
+    def _build(self):
         for i in range(Board.SIZE):
             for j in range(Board.SIZE):
                 if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1):
-                    self.squares[i][j] = Square(Color.DARK)
+                    self.state[j][i] = Square(Color.LIGHT_SQUARE)
                 else:
-                    self.squares[i][j] = Square(Color.LIGHT)
+                    self.state[j][i] = Square(Color.DARK_SQUARE)
 
     def clear(self):
-        for row in self.squares[Board.FILLED_ROWS:-Board.FILLED_ROWS]:
+        for row in self.state[Board.FILLED_ROWS:-Board.FILLED_ROWS]:
             for square in row:
                 if square.is_available():
                     square.piece = None
@@ -35,19 +40,24 @@ class Board:
         self.dark_pieces = []
 
     def prepare_pieces(self):
-        for row in self.squares[:Board.FILLED_ROWS]:
+        for row in self.state[:Board.FILLED_ROWS]:
             for square in row:
                 if square.is_available():
-                    piece = Piece(Color.LIGHT)
+                    piece = Piece(Color.LIGHT_PIECE)
                     square.piece = piece
                     self.light_pieces.append(piece)
 
-        for row in self.squares[-Board.FILLED_ROWS:]:
+        for row in self.state[-Board.FILLED_ROWS:]:
             for square in row:
                 if square.is_available():
-                    piece = Piece(Color.DARK)
+                    piece = Piece(Color.DARK_PIECE)
                     square.piece = piece
                     self.dark_pieces.append(piece)
+
+    def draw(self, surface):
+        for i in range(Board.SIZE):
+            for j in range(Board.SIZE):
+                self.state[i][j].draw(surface, j, i)
 
 
 class Square:
@@ -59,7 +69,25 @@ class Square:
         return self.piece is not None
 
     def is_available(self):
-        return self._color is not Color.LIGHT
+        return self._color is not Color.LIGHT_SQUARE
+
+    def draw(self, surface: pygame.Surface, x, y):
+        self._draw_square(surface, x, y)
+        if self.piece:
+            self.piece.draw(surface, x, y)
+
+    def _draw_square(self, surface: pygame.Surface, x, y):
+        w = surface.get_width()
+        h = surface. get_height()
+        board_size = min(w, h)
+        square_size = int(board_size / Board.SIZE)
+        square_rect = pygame.Rect((w - board_size) / 2 + x * square_size,
+                                  (h - board_size) / 2 + y * square_size,
+                                  square_size, square_size)
+        pygame.draw.rect(surface, self._color, square_rect)
+
+    def __str__(self):
+        return '{}'.format(self.piece)
 
 
 class Piece:
@@ -70,3 +98,20 @@ class Piece:
     @property
     def is_king(self):
         return self._king
+
+    def draw(self, surface: pygame.Surface, x, y):
+        w = surface.get_width()
+        h = surface.get_height()
+        board_size = min(w, h)
+        piece_radius = int((board_size / Board.SIZE) / 2)
+        piece_pos = (
+            int((w - board_size) / 2 + (x + 0.5) * (piece_radius * 2)),
+            int((h - board_size) / 2 + (y + 0.5) * (piece_radius * 2))
+        )
+        pygame.draw.circle(surface, self._color, piece_pos, piece_radius)
+
+    def __str__(self):
+        if self._color is Color.DARK_PIECE:
+            return 'White'
+        elif self._color is Color.LIGHT_PIECE:
+            return 'Black'
--- pycheckers/pycheckers.py
+++ pycheckers/pycheckers.py
@@ -1,21 +1,27 @@
 import pygame
 
+from elements import Board
+
+
 class Game:
-    def __init__(self, clock, screen, max_fps=60):
+    def __init__(self, clock, surface: pygame.Surface, max_fps=60):
         self._clock = clock
-        self._screen = screen
+        self._surface = surface
         self._max_fps = max_fps
+        self._board = Board()
 
     def run(self):
         while True:
             clock.tick(self._max_fps)
+            self._board.draw(self._surface)
+            pygame.display.update()
 
 
 if __name__ == '__main__':
     pygame.init()
 
     clock = pygame.time.Clock()
-    resolution = (640, 480)
+    resolution = (500, 500)
     screen = pygame.display.set_mode(resolution, pygame.HWSURFACE | pygame.DOUBLEBUF)
     pygame.display.set_caption('PyCheckers')
 

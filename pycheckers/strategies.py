8e3047392ae6b77b57cfa8974a111723d379b33e
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-05-08 00:52:02 +0200

    Add random game strategy and bit of validation logic in base game strategy class

--- pycheckers/strategies.py
+++ pycheckers/strategies.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
-from elements import Board
+import random
+from time import sleep
+
+from elements import Board, Piece, Color
 from heuristics import light_pieces_dark_pieces_difference_heuristic
 
 
@@ -8,6 +11,27 @@ class GameStrategy(ABC):
     @abstractmethod
     def move(self, board: Board):
         pass
+
+    def _calculate_valid_moves(self, piece: Piece, board: Board):
+        if piece._color == Color.LIGHT_PIECE:
+            moves = []
+            if piece.is_king:
+                # TODO Implement king moves logic
+                pass
+            else:
+                piece_x, piece_y = board.position(piece)
+                forward_positions = [(piece_x - 1, piece_y - 1), (piece_x + 1, piece_y - 1)]
+                backward_positions = [(piece_x - 1, piece_y + 1), (piece_x + 1, piece_y + 1)]
+
+                for x, y in forward_positions:
+                    if board.is_in_bounds(x, y) and not board.is_occupied(x, y):
+                        next_state = board.state
+                        next_state[piece_y][piece_x] = None
+                        next_state[y][x] = piece
+                        moves.append(next_state)
+
+            return moves
+
 
 
 class AlphaBetaGameStrategy(GameStrategy):
@@ -27,3 +51,11 @@ class MiniMaxGameStrategy(GameStrategy):
 
     def move(self, board: Board):
         pass
+
+class RandomGameStrategy(GameStrategy):
+    def move(self, board: Board):
+        moves = []
+        for piece in board.light_pieces:
+            moves += self._calculate_valid_moves(piece, board)
+        board.state = random.choice(moves)
+        sleep(1)
